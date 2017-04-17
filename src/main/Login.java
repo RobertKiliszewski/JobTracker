032d8da79609b4d7a72c8dfba74e1debffee08ad
RobertKiliszewski <robert.kiliszewski@hotmail.com>
2017-04-17 16:26:21 +0100

    Login
    Added TextFields and Buttons

--- src/main/Login.java
+++ src/main/Login.java
@@ -1,13 +1,20 @@
 package main;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Login {
 
 	private JFrame frame;
+	private JTextField username;
+	private JTextField password;
 
+	
 	/**
 	 * Launch the application.
 	 */
@@ -38,6 +45,34 @@ public class Login {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		//Username
+		username = new JTextField();
+		username.setBounds(175, 85, 86, 20);
+		frame.getContentPane().add(username);
+		username.setColumns(10);
+		
+		//Password
+		password = new JTextField();
+		password.setBounds(175, 130, 86, 20);
+		frame.getContentPane().add(password);
+		password.setColumns(10);
+		
+		//Exit
+		JButton exitbtn = new JButton("Exit");
+		exitbtn.setBounds(10, 227, 89, 23);
+		frame.getContentPane().add(exitbtn);
+	
+		exitbtn.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent e) {
+			    System.exit(0);
+			  }
+			});
+		
+		//Login
+		JButton loginbtn = new JButton("Login");
+		loginbtn.setBounds(335, 227, 89, 23);
+		frame.getContentPane().add(loginbtn);
 	}
-
 }
